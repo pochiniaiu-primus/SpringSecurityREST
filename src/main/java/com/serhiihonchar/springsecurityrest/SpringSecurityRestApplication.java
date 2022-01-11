04bd0a1638d9d5b4eca2277edec73008cc03a8ce
Serhii Honchar <sehonc@softserveinc.com>
2022-01-11 13:34:39 +0200

    app configured

---
+++ src/main/java/com/serhiihonchar/springsecurityrest/SpringSecurityRestApplication.java
@@ -0,0 +1,12 @@
+package com.serhiihonchar.springsecurityrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRestApplication.class, args);
+    }
+}
