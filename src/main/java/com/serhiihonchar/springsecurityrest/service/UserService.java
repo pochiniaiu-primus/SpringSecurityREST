ab2d3e2c37027b581e9c5dc865e3d78e0d01985b
Serhii Honchar <sehonc@softserveinc.com>
2022-01-12 23:49:41 +0200

    add user register

---
+++ src/main/java/com/serhiihonchar/springsecurityrest/service/UserService.java
@@ -0,0 +1,17 @@
+package com.serhiihonchar.springsecurityrest.service;
+
+import com.serhiihonchar.springsecurityrest.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User register(User user);
+
+    List<User> getAll();
+
+    User findByUsername(String username);
+
+    User findById(Long id);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com/serhiihonchar/springsecurityrest/service/impl/UserServiceImpl.java
@@ -0,0 +1,67 @@
+package com.serhiihonchar.springsecurityrest.service.impl;
+
+import com.serhiihonchar.springsecurityrest.model.Role;
+import com.serhiihonchar.springsecurityrest.model.Status;
+import com.serhiihonchar.springsecurityrest.model.User;
+import com.serhiihonchar.springsecurityrest.repository.RoleRepository;
+import com.serhiihonchar.springsecurityrest.repository.UserRepository;
+import com.serhiihonchar.springsecurityrest.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User register(User user) {
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles = new ArrayList<>();
+        userRoles.add(roleUser);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
+
+        User registeredUser = userRepository.save(user);
+        log.info("IN register - user: {} successfully registered", registeredUser);
+
+        return registeredUser;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
